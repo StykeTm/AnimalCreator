bc69c9e910ee42a2adaa85f7f3a7af549cc44503
styketm <jony.jimenezz@gmail.com>
2024-08-30 17:20:48 -0400

    Agregadas funciones y nuevas variables

--- animal.js
+++ animal.js
@@ -1,5 +1,18 @@
 const animales = ['Perro', 'Gato', 'Gallina', 'Vaca', 'Caballo', 'Cerdo'];
 let patas = Math.floor(Math.random() * 10);
 const colores= ['Rojo', 'Azul', 'Verde', 'Morado', 'Rosado', 'Negro', 'Gris', 'Blanco', 'Amarillo',]
-const ojos = color[i].length;
+
+
+let animal = () => {
+    let numero = Math.floor(Math.random() * animales.length);
+    return animales[numero];
+}
+
+let color = () => {
+    let num = Math.floor(Math.random() * colores.length);
+    return colores[num];
+}
+let pet = animal();
+let col = color();
+let ojos = col.length;
 
