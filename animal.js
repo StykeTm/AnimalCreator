2b3846ad2cdaa3aaeadfa6e713740173e0fe5349
styketm <jony.jimenezz@gmail.com>
2024-08-30 17:06:37 -0400

    Variables iniciales

---
+++ animal.js
@@ -0,0 +1,5 @@
+const animales = ['Perro', 'Gato', 'Gallina', 'Vaca', 'Caballo', 'Cerdo'];
+let patas = Math.floor(Math.random() * 10);
+const colores= ['Rojo', 'Azul', 'Verde', 'Morado', 'Rosado', 'Negro', 'Gris', 'Blanco', 'Amarillo',]
+const ojos = color[i].length;
+
